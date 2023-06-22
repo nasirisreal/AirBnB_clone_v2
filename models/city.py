ab8feb2b7117927ea7bb004eed0aaecfb9d5074b
nasirisreal <nasirisreal@yahoo.com>
2023-06-22 01:14:17 -0700

    AirBnB_clone_v2

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage_type
+from models import storage
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -9,11 +9,11 @@ from sqlalchemy.orm import relationship
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    if storage_type == 'db':
+    if storage == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities',
-                              cascade='all, delete, delete-orphan')
+        places = relationship('Place', backref='cities', cascade='all, delete, delete-orphan')
     else:
         name = ''
         state_id = ''
+
