86d522a3ec81fe4a482b7990833dd1d3b40b489c
nasirisreal <nasirisreal@yahoo.com>
2023-07-10 00:22:36 -0700

    Adding a Puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -11,7 +11,7 @@ $nginx_conf = "server {
         index index.html index.htm;
     }
     location /redirect_me {
-        return 301 https://hbibidunni.com/;
+        return 301 https://learningcodes.tech/;
     }
     error_page 404 /404.html;
     location /404 {
